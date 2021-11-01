39171993091cde29f98befc95d2136649d6659c5
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-11-01 15:05:02 +0300

    Add base Random class

---
+++ Assets/Scripts/Random/Random.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Random {
+    public class Random
+    {
+        private readonly System.Random _rnd;
+        public int Seed { get; }
+        
+        public Random(int seed) {
+            Seed = seed;
+            _rnd = new System.Random(seed);
+        }
+
+        public int NextInt(int min, int max) {
+            return _rnd.Next(min, max);
+        }
+
+        public float NextFloat(float min, float max) {
+            return min + (float) _rnd.NextDouble() * (max - min);
+        }
+
+        public T NextChoice<T>(IReadOnlyList<T> collection) {
+            var idx = NextInt(0, collection.Count);
+            return collection[idx];
+        }
+
+        public List<T> NextShuffle<T>(IEnumerable<T> collection) {
+            return collection.OrderBy(_ => NextFloat(0, 1)).ToList();
+        }
+    }
+}
--- Assets/Scripts/TestBehaviour.cs
+++ Assets/Scripts/TestBehaviour.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class TestBehaviour : MonoBehaviour {
-    void Start() {
-        Debug.Log("wow");
+    private void Start() {
+        var rng = new Random.Random(UnityEngine.Random.Range(0, int.MaxValue));
+        Debug.Log($"Seed: {rng.Seed}, float: {rng.NextFloat(0, 100)}");
     }
 }
