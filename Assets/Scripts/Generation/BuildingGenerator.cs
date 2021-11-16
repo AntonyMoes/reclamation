16427d116d9df2d1f03e022dd54f39be1f8e8c5b
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-11-17 00:05:40 +0300

    Building generation WIP

---
+++ Assets/Scripts/Generation/BuildingGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Generation {
+    public class BuildingGenerator {
+        public BuildingGenerator(/*list of rooms and specifications for a building type*/) { }
+
+        public void Generate(/*list of conditions for a specific building*/) {
+            /*
+             * 1. Get surrounding conditions and generate come constraints
+             * 2. Generate a list of functional blocks
+             * 3. For each block generate a list of rooms, getting them pools corresponding to pool types,
+             *      get constraints for this block
+             * 4. Place blocks
+             * 5. Place rooms
+             * ??? 6. Optimize placings, corridors and routes
+             */
+        }
+    }
+}
